2065f5b00de9778248578a3e690a837bc46a173d
traBpUkciP <scott.doucet@hotmail.com>
2018-01-30 14:13:20 -0400

    Add a 'Turn off notifications' button to the default message.
    No more messing with the configuration manually :tada:

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,35 @@
 import * as vscode from 'vscode';
-import { Terminal } from 'vscode';
+import { Terminal, MessageItem } from 'vscode';
 import { ScriptNodeProvider } from './npmScripts'
 
 export function activate(context: vscode.ExtensionContext) {
-  const terminalMap = new Map<string, Terminal>();
-  
-	vscode.window.registerTreeDataProvider('npmScripts', new ScriptNodeProvider(vscode.workspace.rootPath));
+	const rootPath = vscode.workspace.rootPath || ".";
+
+	const terminalMap = new Map<string, Terminal>();
+	const npmScriptsProvider = new ScriptNodeProvider(rootPath);
+
+	vscode.window.registerTreeDataProvider('npmScripts', npmScriptsProvider);
 	vscode.window.onDidCloseTerminal(term => terminalMap.delete(term.name));
-  
+
 	vscode.commands.registerCommand('npmScripts.executeCommand', task => {
-    const packageManager = vscode.workspace.getConfiguration('npm').get('packageManager') || 'npm';
+		const packageManager = vscode.workspace.getConfiguration('npm').get('packageManager') || 'npm';
 		const command = `${packageManager} run ${task}`;
-    
-    const config = vscode.workspace.getConfiguration('npm-scripts')
-    config['showStartNotification'] && vscode.window.showInformationMessage(command);
+
+		const config = vscode.workspace.getConfiguration('npm-scripts')
+		if (config['showStartNotification']) {
+
+			const hideMessages: MessageItem = { title: "Hide messages" };
+			vscode.window.showInformationMessage(command, hideMessages)
+				.then(result => {
+					if (result === hideMessages) {
+						config.update('showStartNotification', false, false);
+						vscode.window.showInformationMessage([
+							"To turn NPM-Scripts notifications back on, set",
+							"\"showStartNotification\" to 'true' under User Options"
+						].join(' '))
+					}
+				})
+		}
 
 		let terminal: Terminal;
 		if (terminalMap.has(task)) {
@@ -25,5 +41,5 @@ export function activate(context: vscode.ExtensionContext) {
 
 		terminal.show();
 		terminal.sendText(command);
-  });
+	});
 }
